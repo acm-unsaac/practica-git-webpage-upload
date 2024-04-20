a4c969b57ae518f5aa0968654edd6edcf2161587
Jus <>
2024-04-19 19:07:10 -0500

    adding styles

---
+++ Justino/app.js
@@ -0,0 +1,8 @@
+var menuItems = document.querySelectorAll(".menu-item");
+for (var i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
